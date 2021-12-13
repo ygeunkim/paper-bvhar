a383f6fa51e4e1878b2c23ffbb71447cfc3dc32b
Young Geun Kim <dudrmshub@gmail.com>
2021-12-13 23:59:58 +0900

    move bvar simulation to separate file

---
+++ r/simulation-bvar.R
@@ -0,0 +1,127 @@
+if (requireNamespace("tidyverse", quietly = TRUE)) {
+  library(tidyverse)
+}
+if (requireNamespace("bvhar", quietly = TRUE)) {
+  library(bvhar)
+}
+# hyperparameter setting table------------------------
+source("R/param-fns.R")
+set.seed(1)
+# Minnesota prior-------------------------------------
+# p = 5
+# SMALL: 3
+# MEDIUM: 9
+# LARGE: 12
+# Random walk: delta = 1
+# sigma = 1
+# lambda = 3
+#-----------------------------------------------------
+bvar_lag <- 5
+num_train <- 1000
+num_test <- 100
+num_burin <- 300
+# SMALL-----------------------------------------------
+n_small <- 3
+bvar_small_spec <- set_bvar(
+  sigma = rep(.05, n_small),
+  lambda = .2,
+  delta = rep(0, n_small)
+)
+# tex source for SMALL spec--------------
+bvar_small_spec %>% 
+  kable_hyperparam(caption = "\\label{tab:smallbvarspec}BVAR Coefficient Simulation Setting - SMALL") %>% 
+  writeLines()
+# generate SMALL coef--------------------
+set.seed(1)
+bvar_small_coef <- sim_mncoef(bvar_lag, bvar_small_spec)
+# SMALL dataset--------------------------
+set.seed(1)
+y_small <- sim_var(
+  num_train + num_test,
+  num_burin,
+  bvar_small_coef$coefficients,
+  bvar_lag,
+  bvar_small_coef$covmat,
+  matrix(0L, nrow = bvar_lag, ncol = n_small)
+) %>% 
+  as.data.frame()
+colnames(y_small) <- paste("asset", sprintf(1:n_small, fmt = "%02d"), sep = "_")
+# Split---------------------------------
+y_small_split <- divide_ts(y_small, num_test)
+# MEDIUM----------------------------------------------
+n_medium <- 9
+bvar_medium_spec <- set_bvar(
+  sigma = rep(.05, n_medium),
+  lambda = .1,
+  delta = rep(0, n_medium)
+)
+# tex source for MEDIUM coef----------
+bvar_medium_spec %>% 
+  kable_hyperparam(caption = "\\label{tab:medbvarspec}BVAR Coefficient Simulation Setting - MEDIUM") %>% 
+  writeLines()
+# generate MEDIUM coef----------------
+set.seed(1)
+bvar_medium_coef <- sim_mncoef(bvar_lag, bvar_medium_spec)
+# MEDIUM dataset----------------------
+set.seed(1)
+y_medium <- sim_var(
+  num_train + num_test,
+  num_burin,
+  bvar_medium_coef$coefficients,
+  bvar_lag,
+  bvar_medium_coef$covmat,
+  matrix(0L, nrow = bvar_lag, ncol = n_medium)
+) %>% 
+  as.data.frame()
+colnames(y_medium) <- paste("asset", sprintf(1:n_medium, fmt = "%02d"), sep = "_")
+# Split---------------------------------
+y_medium_split <- divide_ts(y_medium, num_test)
+# LARGE----------------------------------------------
+n_large <- 12
+bvar_large_spec <- set_bvar(
+  sigma = rep(.05, n_large),
+  lambda = .03,
+  delta = rep(0, n_large)
+)
+# tex source for LARGE coef-----------
+bvar_large_spec %>% 
+  kable_hyperparam(caption = "\\label{tab:largebvarspec}BVAR Coefficient Simulation Setting - LARGE") %>% 
+  writeLines()
+# generate LARGE coef-----------------
+set.seed(1)
+bvar_large_coef <- sim_mncoef(bvar_lag, bvar_large_spec)
+# LARGE dataset------------------------
+set.seed(1)
+y_large <- sim_var(
+  num_train + num_test,
+  num_burin,
+  bvar_large_coef$coefficients,
+  bvar_lag,
+  bvar_large_coef$covmat,
+  matrix(0L, nrow = bvar_lag, ncol = n_large)
+) %>% 
+  as.data.frame()
+colnames(y_large) <- paste("asset", sprintf(1:n_large, fmt = "%02d"), sep = "_")
+# Split---------------------------------
+y_large_split <- divide_ts(y_large, num_test)
+# Save----------------------------------------------
+# in data/processed/
+# File name: bvarsim_a_b.csv
+# a: small, medium, large
+# b: train, test
+#---------------------------------------------------
+# # Save SMALL---------------------------
+# y_small_split$train %>%
+#   write_csv(file = "data/processed/bvarsim_small_train.csv")
+# y_small_split$test %>%
+#   write_csv(file = "data/processed/bvarsim_small_test.csv")
+# # Save MEDIUM--------------------------
+# y_medium_split$train %>%
+#   write_csv(file = "data/processed/bvarsim_medium_train.csv")
+# y_medium_split$test %>%
+#   write_csv(file = "data/processed/bvarsim_medium_test.csv")
+# # Save LARGE---------------------------
+# y_large_split$train %>%
+#   write_csv(file = "data/processed/bvarsim_large_train.csv")
+# y_large_split$test %>%
+#   write_csv(file = "data/processed/bvarsim_large_test.csv")
